49ccba2c04b6fa8ae79f02b83a4e1169cb070737
spore <spore2050@gmail.com>
2024-11-23 02:51:59 +0800

    bench: switch to criterion

--- benches/perf.rs
+++ benches/perf.rs
@@ -1,10 +1,6 @@
-#![feature(test)]
-
-extern crate test;
-
+use criterion::{criterion_group, criterion_main, Criterion};
 use serde_json::json;
 use std::ffi::CString;
-use test::Bencher;
 
 const LONG_MAP_WITH_STRINGS: &str = include_str!("perf/long_map_with_strings");
 const REALLY_LONG_FILE: &str = include_str!("perf/really_long_file");
@@ -14,6 +10,7 @@ const REALLY_LONG_FILE_WITH_UNCLOSED_QUOTE: &str =
     include_str!("perf/really_long_file_with_unclosed_quote");
 
 fn build_case(mode: &str, text: &str) -> CString {
+    unsafe { parinfer_rust::INITIALIZED = true };
     CString::new(
         json!({
             "mode": mode,
@@ -29,98 +26,128 @@ fn build_case(mode: &str, text: &str) -> CString {
     .unwrap()
 }
 
-#[bench]
-fn bench_paren_long_map_with_strings(b: &mut Bencher) {
+fn bench_paren_long_map_with_strings(c: &mut Criterion) {
     unsafe {
         let options = build_case("paren", LONG_MAP_WITH_STRINGS);
-        b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()));
+        c.bench_function("bench_paren_long_map_with_strings", |b| {
+            b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()))
+        });
     }
 }
 
-#[bench]
-fn bench_indent_long_map_with_strings(b: &mut Bencher) {
+fn bench_indent_long_map_with_strings(c: &mut Criterion) {
     unsafe {
         let options = build_case("indent", LONG_MAP_WITH_STRINGS);
-        b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()));
+        c.bench_function("bench_indent_long_map_with_strings", |b| {
+            b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()))
+        });
     }
 }
 
-#[bench]
-fn bench_smart_long_map_with_strings(b: &mut Bencher) {
+fn bench_smart_long_map_with_strings(c: &mut Criterion) {
     unsafe {
         let options = build_case("smart", LONG_MAP_WITH_STRINGS);
-        b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()));
+        c.bench_function("bench_smart_long_map_with_strings", |b| {
+            b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()))
+        });
     }
 }
 
-#[bench]
-fn bench_paren_really_long_file(b: &mut Bencher) {
+fn bench_paren_really_long_file(c: &mut Criterion) {
     unsafe {
         let options = build_case("paren", REALLY_LONG_FILE);
-        b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()));
+        c.bench_function("bench_paren_really_long_file", |b| {
+            b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()))
+        });
     }
 }
 
-#[bench]
-fn bench_indent_really_long_file(b: &mut Bencher) {
+fn bench_indent_really_long_file(c: &mut Criterion) {
     unsafe {
         let options = build_case("indent", REALLY_LONG_FILE);
-        b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()));
+        c.bench_function("bench_indent_really_long_file", |b| {
+            b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()))
+        });
     }
 }
 
-#[bench]
-fn bench_smart_really_long_file(b: &mut Bencher) {
+fn bench_smart_really_long_file(c: &mut Criterion) {
     unsafe {
         let options = build_case("smart", REALLY_LONG_FILE);
-        b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()));
+        c.bench_function("bench_smart_really_long_file", |b| {
+            b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()))
+        });
     }
 }
 
-#[bench]
-fn bench_paren_really_long_file_with_unclosed_paren(b: &mut Bencher) {
+fn bench_paren_really_long_file_with_unclosed_paren(c: &mut Criterion) {
     unsafe {
         let options = build_case("paren", REALLY_LONG_FILE_WITH_UNCLOSED_PAREN);
-        b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()));
+        c.bench_function("bench_paren_really_long_file_with_unclosed_paren", |b| {
+            b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()))
+        });
     }
 }
 
-#[bench]
-fn bench_indent_really_long_file_with_unclosed_paren(b: &mut Bencher) {
+fn bench_indent_really_long_file_with_unclosed_paren(c: &mut Criterion) {
     unsafe {
         let options = build_case("indent", REALLY_LONG_FILE_WITH_UNCLOSED_PAREN);
-        b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()));
+        c.bench_function("bench_indent_really_long_file_with_unclosed_paren", |b| {
+            b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()))
+        });
     }
 }
 
-#[bench]
-fn bench_smart_really_long_file_with_unclosed_paren(b: &mut Bencher) {
+fn bench_smart_really_long_file_with_unclosed_paren(c: &mut Criterion) {
     unsafe {
         let options = build_case("smart", REALLY_LONG_FILE_WITH_UNCLOSED_PAREN);
-        b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()));
+        c.bench_function("bench_smart_really_long_file_with_unclosed_paren", |b| {
+            b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()))
+        });
     }
 }
 
-#[bench]
-fn bench_quote_really_long_file_with_unclosed_quote(b: &mut Bencher) {
+fn bench_quote_really_long_file_with_unclosed_quote(c: &mut Criterion) {
     unsafe {
         let options = build_case("paren", REALLY_LONG_FILE_WITH_UNCLOSED_QUOTE);
-        b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()));
+        c.bench_function("bench_quote_really_long_file_with_unclosed_quote", |b| {
+            b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()))
+        });
     }
 }
 
-#[bench]
-fn bench_indent_really_long_file_with_unclosed_quote(b: &mut Bencher) {
+fn bench_indent_really_long_file_with_unclosed_quote(c: &mut Criterion) {
     unsafe {
         let options = build_case("indent", REALLY_LONG_FILE_WITH_UNCLOSED_QUOTE);
-        b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()));
+        c.bench_function("bench_indent_really_long_file_with_unclosed_quote", |b| {
+            b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()))
+        });
     }
 }
 
-#[bench]
-fn bench_smart_really_long_file_with_unclosed_quote(b: &mut Bencher) {
+fn bench_smart_really_long_file_with_unclosed_quote(c: &mut Criterion) {
     unsafe {
         let options = build_case("smart", REALLY_LONG_FILE_WITH_UNCLOSED_QUOTE);
-        b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()));
+        c.bench_function("bench_smart_really_long_file_with_unclosed_quote", |b| {
+            b.iter(|| parinfer_rust::run_parinfer(options.as_ptr()))
+        });
     }
 }
+
+criterion_group!(
+    benches,
+    bench_paren_long_map_with_strings,
+    bench_indent_long_map_with_strings,
+    bench_smart_long_map_with_strings,
+    bench_paren_really_long_file,
+    bench_indent_really_long_file,
+    bench_smart_really_long_file,
+    bench_paren_really_long_file_with_unclosed_paren,
+    bench_indent_really_long_file_with_unclosed_paren,
+    bench_smart_really_long_file_with_unclosed_paren,
+    bench_quote_really_long_file_with_unclosed_quote,
+    bench_indent_really_long_file_with_unclosed_quote,
+    bench_smart_really_long_file_with_unclosed_quote,
+);
+
+criterion_main!(benches);
